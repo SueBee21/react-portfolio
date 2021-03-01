844f9e4641b86b61ece1d15d1aa574c34a5a6ab1
SusanBeno <beno.susan@gmail.com>
2021-03-01 07:36:19 -0500

    issues

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header () {
+    return (
+        <div className = "text-center">
+            <h1>Susan L Beno</h1>
+            <h2>Full Stack Web Developer</h2>
+        </div>
+    )
+}
+export default Header;
---
+++ src/Components/Jumbotron/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Jumbotron(props) {
+  return <div className="bg-light">{props.children}</div>;
+}
+export default Jumbotron;
