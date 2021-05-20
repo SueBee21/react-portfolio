4c2da7b69c3c3c345ba89af192d5a5b6f570009c
SusanBeno <beno.susan@gmail.com>
2021-05-20 07:37:34 -0400

    working on navbar

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Nav, Navbar, Container} from 'react-bootstrap';
  
 const Navigation = () => {
     return (
        <div>
-          <NavLink to="/">Home</NavLink>
+
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">Susan Beno</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+    <NavLink to="/">Home</NavLink>
           <NavLink to="/contact">Contact</NavLink>
           <NavLink to="/portfolio">Portfolio</NavLink>
+    </Nav>
+
+  </Navbar.Collapse>
+</Navbar>
+
+
+
+
+
+
+
        </div>
     );
 }
